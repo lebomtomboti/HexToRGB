8c17483a5aeec9330825b020b1652bb6b8b8b940
lebomtomboti <lebomtomboti17@gmail.com>
2024-06-05 09:00:07 +0200

    first commit

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
+function convertRGBtoHex() {
+    let redValue = parseInt(document.getElementById("red").value);
+    let greenValue = parseInt(document.getElementById("green").value);
+    let blueValue = parseInt(document.getElementById("blue").value);
+  
+    let redHex = redValue.toString(16).padStart(2, "0");
+    let greenHex = greenValue.toString(16).padStart(2, "0");
+    let blueHex = blueValue.toString(16).padStart(2, "0");
+  
+    
+    let hexColor = "#" + redHex + greenHex + blueHex;
+  
+    document.getElementById("result").innerHTML = "Hexadecimal color: " + hexColor;
+  }
+  
   
